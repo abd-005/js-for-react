811da75291f09e86ea29c832259d66dbffc64857
abd-005 <abd.me7@gmail.com>
2025-09-17 19:46:14 +0600

    Added Shortcut

---
+++ shortcut.js
@@ -0,0 +1,42 @@
+// 'almas', 5, true, {}, []
+// '', 0, false, null, undefined
+
+// check truthy
+
+// const test = undefined
+//  if(test){
+//  console.log("It's Truthy")
+//  }
+//  else{
+//  console.log("It's falsy")
+//  }
+
+
+// --------ternary------------
+
+// test ? console.log("It's Truthy") : console.log("It's falsy");
+
+// let test = 50;
+
+// test = true ? 10 : 30;
+
+// ---- 
+
+// const isActive = true;
+
+// const showActive = () => {
+//     console.log("Mewo is awake")
+// }
+// const hideActive = () => {
+//     console.log("Mewo is sleeping")
+// }
+
+// isActive ? showActive() : hideActive()
+// isActive && showActive()
+// isActive || hideActive()
+// console.log(isActive)
+
+
+// string conversion
+
+
